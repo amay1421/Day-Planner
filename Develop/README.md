cc882539022c3fb3bea71740ca82473c97910278
Alyson May <amay1421@yahoo.com>
2021-01-12 15:15:15 -0600

    Read Me Update1

---
+++ Develop/README.md
@@ -0,0 +1,10 @@
+# Day-Planner
+
+This a personal day planner that allows users to schedule their work day. When a user opens the browser they will see the current day and time, along with color coded time blocks. The grey represents the past, the red represents the current hour, and the green represents the future. The user can add text to each time block and save it to their local storage.
+
+![Day Planner](Day_planner.png)
+
+<strong>Deployed Link:</strong>
+<br>
+<strong>Repo Link:</strong>
+
--- Develop/script.js
+++ Develop/script.js
@@ -12,15 +12,13 @@ var currentData = JSON.parse(localStorage.getItem('DaySchedule')) || [];
 
 // get all input boxes
 var inputBoxes = $(".textarea")
-console.log(inputBoxes)
 
 // color them accordingly
 for (let index = 0; index < inputBoxes.length; index++) {
     
     var thisHour = parseInt($(inputBoxes[index]).attr("id"));
 
-    //var currentHour = parseInt(moment().format('H'));
-    var currentHour = 14;
+    var currentHour = parseInt(moment().format('H'));
 
     console.log(thisHour + ' vs. ' + currentHour + ' || ' + (thisHour < currentHour))
     if(thisHour < currentHour) {
@@ -52,19 +50,5 @@ for (let index = 0; index < saveButtons.length; index++) {
 
         localStorage.setItem('DaySchedule', JSON.stringify(currentData))
 
-
     })
 }
-
-$('#planner-form').dayScheduleSelector({
-
-})
-
-
-//When i open the page, the day planner is color coded to tell the user whether its past, present or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
